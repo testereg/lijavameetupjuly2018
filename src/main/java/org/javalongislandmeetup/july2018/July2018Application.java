0a5e947290e9e009891222cd515697ca999b359a
Larry Diamond <larry.diamond@coxautoinc.com>
2018-07-12 22:00:33 -0400

    cleanup

--- src/main/java/org/javalongislandmeetup/july2018/July2018Application.java
+++ src/main/java/org/javalongislandmeetup/july2018/July2018Application.java
@@ -3,12 +3,6 @@ package org.javalongislandmeetup.july2018;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-
 @SpringBootApplication
 public class July2018Application {
 
@@ -16,10 +10,4 @@ public class July2018Application {
 		SpringApplication.run(July2018Application.class, args);
 	}
 
-
-		@RequestMapping(value = "/getfiles", method = RequestMethod.GET)
-		public String getS3Files() {
-			System.out.println ("GetS3");
-	    return "blah";
-	  }
 }
